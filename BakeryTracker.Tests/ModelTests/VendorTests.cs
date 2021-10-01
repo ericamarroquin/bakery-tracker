80250d3d91a70d53fbe3922859e81e8ce3de683b
EricaMarroquin <ericamarroquin03@gmail.com>
2021-10-01 11:56:08 -0700

    add passing test to create Vendor instance

---
+++ BakeryTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace BakeryTracker.Tests
+{
+  [TestClass]
+  public class VendorTests //: IDisposable
+  {
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ BakeryTracker/Models/Vendor.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace BakeryTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _vendors = new List<Vendor> {};
+    public string VendorName { get; set; }
+    public string VendorDescription { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string name, string description)
+    {
+      VendorName = name;
+      VendorDescription = description;
+      _vendors.Add(this);
+      Id = _vendors.Count;
+      Orders = new List<Order>{};
+    }
+
+    public static void ClearAll()
+    {
+      _vendors.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _vendors;
+    }
+  }
+}
